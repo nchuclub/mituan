8b657b024acfec66c7da806b4253f5a9c00fba1d
RDS <1096473720@qq.com>
2022-05-03 17:52:19 +0800

    Initial commit

---
+++ src/main/java/com/rds/mituan/MituanApplication.java
@@ -0,0 +1,13 @@
+package com.rds.mituan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MituanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MituanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rds/mituan/MituanApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rds.mituan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MituanApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
